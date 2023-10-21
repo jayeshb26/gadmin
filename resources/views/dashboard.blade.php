9df6bb809d124b311d83834d541e93c909f21432
Dhanesh Joshi <hydradhanesh@gmail.com>
2023-10-21 16:05:48 +0530

    updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -265,8 +265,8 @@
 
     <div class="row">
         @if (Session::get('role') == 'Admin')
-            // Session::get('role') == 'super_distributor' ||
-            //Session::get('role') == 'distributor'
+            {{--  // Session::get('role') == 'super_distributor' ||
+            //Session::get('role') == 'distributor'  --}}
             @if (Session::get('is_f') == 'true')
                 <div class="col-md-6 col-lg-4 grid-margin stretch-card">
                     <div class="card">
