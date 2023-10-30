e9681d6f5aff61a9b6158fed969f1716637de276
Dhanesh Joshi <hydradhanesh@gmail.com>
2023-10-30 12:07:47 +0530

    updated view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -291,65 +291,63 @@
             </div>  --}}
             <div class="col-lg-3 col-md-4 col-sm-4 grid-margin stretch-card">
                 <div class="card bg-warning ">
-                    <a href="{{ url('/generatePointList') }}">
-                        <div class="card-body">
-                            <div class=" row">
-                                <div class="col-md-8">
-                                    <h6 class="text-white mb-2">Today PlayPoints</h6>
-                                    <div>
-                                        <h3 class="text-white">
-                                            {{ number_format($data['tPlayPoint']) }}
-                                        </h3>
-                                    </div>
-                                </div>
-                                <div class="col-md-4 mt-1">
-                                    <h1 class="text-white text-right mr-3"><i class="fa fa-user"></i></h1>
+                    <div class="card-body">
+                        <div class=" row">
+                            <div class="col-md-8">
+                                <h6 class="text-white mb-2">Today PlayPoints</h6>
+                                <div>
+                                    <h3 class="text-white">
+                                        {{ number_format($data['tPlayPoint']) }}
+                                    </h3>
                                 </div>
                             </div>
+                            <div class="col-md-4 mt-1">
+                                <h1 class="text-white text-right mr-3"><i class="fa fa-user"></i></h1>
+                            </div>
                         </div>
-                    </a>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-4 grid-margin stretch-card">
                 <div class="card bg" style="background-color: #ff2890">
-                    <a href="{{ url('/generatePointList') }}">
-                        <div class="card-body">
-                            <div class=" row">
-                                <div class="col-md-8">
-                                    <h6 class="text-white mb-2">Today WinPoints</h6>
-                                    <div>
-                                        <h3 class="text-white">
-                                            {{ number_format($data['tWinPoint']) }}
-                                        </h3>
-                                    </div>
-                                </div>
-                                <div class="col-md-4 mt-1">
-                                    <h1 class="text-white text-right mr-3"><i class="fa fa-user"></i></h1>
+
+                    <div class="card-body">
+                        <div class=" row">
+                            <div class="col-md-8">
+                                <h6 class="text-white mb-2">Today WinPoints</h6>
+                                <div>
+                                    <h3 class="text-white">
+                                        {{ number_format($data['tWinPoint']) }}
+                                    </h3>
                                 </div>
                             </div>
+                            <div class="col-md-4 mt-1">
+                                <h1 class="text-white text-right mr-3"><i class="fa fa-user"></i></h1>
+                            </div>
                         </div>
-                    </a>
+                    </div>
+
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-4 grid-margin stretch-card">
                 <div class="card bg-danger">
-                    <a href="{{ url('/generatePointList') }}">
-                        <div class="card-body">
-                            <div class=" row">
-                                <div class="col-md-8">
-                                    <h6 class="text-white mb-2">Today End Points</h6>
-                                    <div>
-                                        <h3 class="text-white">
-                                            {{ number_format($data['tEndPoint']) }}
-                                        </h3>
-                                    </div>
-                                </div>
-                                <div class="col-md-4 mt-1">
-                                    <h1 class="text-white text-right mr-3"><i class="fa fa-user"></i></h1>
+
+                    <div class="card-body">
+                        <div class=" row">
+                            <div class="col-md-8">
+                                <h6 class="text-white mb-2">Today End Points</h6>
+                                <div>
+                                    <h3 class="text-white">
+                                        {{ number_format($data['tEndPoint']) }}
+                                    </h3>
                                 </div>
                             </div>
+                            <div class="col-md-4 mt-1">
+                                <h1 class="text-white text-right mr-3"><i class="fa fa-user"></i></h1>
+                            </div>
                         </div>
-                    </a>
+                    </div>
+
                 </div>
             </div>
         @endif
